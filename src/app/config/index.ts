9b20598f552c31a814f8c80096a3384666129082
Pawan <pawanbajaj2012@gmail.com>
2025-03-21 13:06:00 +0530

    feat: add new networks

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,14 +1,50 @@
 type ChainInfo = {
-    [key: string]: {
-      chainId: number;
-      rpcUrl: string;
-    };
+  [key: string]: {
+    chainId: number;
+    rpcUrl: string;
   };
+};
 
 export const chainInfo: ChainInfo = {
 
-    monadTestnet: {
-        chainId: 10143,
-        rpcUrl: 'https://testnet-rpc.monad.xyz/'
-    }
-  };
+  monadTestnet: {
+    chainId: 10143,
+    rpcUrl: 'https://testnet-rpc.monad.xyz/'
+  },
+  ethereum: {
+    chainId: 1,
+    rpcUrl: 'https://ethereum-rpc.publicnode.com'
+  },
+  base: {
+    chainId: 8453,
+    rpcUrl: 'https://mainnet.base.org'
+  },
+  polygon: {
+    chainId: 137,
+    rpcUrl: 'https://polygon-rpc.com/'
+  },
+  arbitrum: {
+    chainId: 42161,
+    rpcUrl: 'https://arb1.arbitrum.io/rpc',
+  },
+  opmainnet: {
+    chainId: 10,
+    rpcUrl: 'https://mainnet.optimism.io'
+  },
+  bnb: {
+    chainId: 56,
+    rpcUrl: 'https://bsc-dataseed1.binance.org/'
+  },
+  celo: {
+    chainId: 42220,
+    rpcUrl: 'https://docs.celo.org/network/node/forno'
+  },
+  scroll: {
+    chainId: 534352,
+    rpcUrl: 'https://rpc.scroll.io/'
+  },
+  mantle: {
+    chainId: 5000,
+    rpcUrl: 'https://rpc.mantle.xyz/'
+  }
+};
